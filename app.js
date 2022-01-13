3a0028966622815312e8889420d2306b34b89e82
FreeFallRush <queenofcups.altbot@gmail.com>
2022-01-13 14:37:38 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,87 +1,91 @@
-//Rock Paper Scissors 
-
-let tieScore = 0;
+let buttons = document.querySelectorAll('.btn');
+let roundCount = 0;
 let userPoints = 0;
 let computerPoints = 0;
 
-
-//show user choice
-function userPlay() {
-  let answer = prompt("Rock? Paper? Scissors?", "");
-  //makes user choice case insensitive
-  let userChoice = answer.toLowerCase();
-  
-  if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors"){
-      alert(`You choose ${userChoice}`);
-      return userChoice;
-      
-  }else{
-      let alertMessage = prompt(`Dude, there are only three options: rock, paper, scissors. Just pick one of them if you want to play`, "");    
-      return alertMessage;
-  } 
-    
-}
-
-//show computer choice
+//computer choice
 function computerPlay() {
-  let choice = ["rock", "paper", "scissors"];
-  let computerChoice = choice[Math.floor(Math.random() * choice.length)];
-  alert(`The computer choose ${computerChoice}`);
-  return computerChoice;   
-   
+    let choice = ['rock', 'paper', 'scissors'];
+    let computerChoice = choice[Math.floor(Math.random() * choice.length)];
+    return computerChoice;
 }
 
-//Single round
-function playRound() {
-  const playerSelection = userPlay();
-  const computerSelection = computerPlay();
-  if(playerSelection === computerSelection) {
-      alert (`It's a tie!`);
-      return tieScore;
-  }else if (
-      playerSelection === "rock" && computerSelection === "scissors"
-   || playerSelection === "scissors" && computerSelection === "paper"
-   || playerSelection === "paper" && computerSelection === "rock"
-  ){
-      alert(`You win the round! ${playerSelection} beats ${computerSelection}`);
-      return userPoints++;
-  }else if(
-      playerSelection === "scissors" && computerSelection === "rock"
-   || playerSelection === "paper" && computerSelection === "scissors" 
-   || playerSelection === "rock" && computerSelection === "paper"
-  ){
-      alert(`You lost the round! ${computerSelection} beats ${playerSelection}`);
-      return computerPoints++;
-  }else{
-      let message = `My bad! I've messed the code`;
-      return message; 
-  }
+//one RPS round 
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
+    let result = "";
+
+    if(playerSelection === computerSelection) {
+        result = (`Your choice:  ${playerSelection}. <br/>
+                   Computer's choice: ${computerSelection}. <br/>
+                   It's a tie!`);
+        roundCount++;
+    } else if (
+        playerSelection === "rock" && computerSelection === "scissors"
+     || playerSelection === "scissors" && computerSelection === "paper"
+     || playerSelection === "paper" && computerSelection === "rock"
+     ) {
+         result = (`Your choice:  ${playerSelection}. <br/>
+                    Computer's choice: ${computerSelection}. <br/>
+                    You win the round! ~ ${playerSelection} beats ${computerSelection} ~`);
+        roundCount++;
+        userPoints++;
+     } else if (
+        playerSelection === "scissors" && computerSelection === "rock"
+     || playerSelection === "paper" && computerSelection === "scissors" 
+     || playerSelection === "rock" && computerSelection === "paper"
+     ) {
+         result = (`Your choice:  ${playerSelection}. <br/> 
+                    Computer's choice: ${computerSelection}. <br/>                   
+                    You lost the round! ~ ${computerSelection} beats ${playerSelection} ~ `);
+        roundCount++;
+        computerPoints++;
+     } else {
+        let message = `My bad! I've messed the code`;
+        message;
+     }
+     document.getElementById('result').innerHTML = result;
+     return;
 }
 
-//Game with 5 rounds using the single round
 
+//hides the RPS buttons once the 5 round game is over
+function hideButtons() {
+    document.getElementById("options").style.display = 'none';
+}
+
+//shows the reset button once the 5 round game is over
+function showResetButton() {
+    document.getElementById("reset").style.display = 'block';
+}
+
+//shows the winner of the game after 5 rounds
 function game() {
-    let roundCount = 5;
+    document.getElementById('round').textContent = roundCount;
+    document.getElementById('player-score').textContent = userPoints;
+    document.getElementById('computer-score').textContent = computerPoints;
+    let gameWinner = "";
 
-    //loop through 5 rounds and alert points after each round
-    for(let i = 1; i<= roundCount; i++) {
-        let roundResult = playRound();
-        alert(`Round ${i} - Human: ${userPoints} points ~ Computer: ${computerPoints} points`);
+    if(roundCount === 5 && userPoints > computerPoints) {
+        gameWinner = (`You win the game! Congrats!`);
+        hideButtons();
+        showResetButton(); 
+    } else if(roundCount === 5 && computerPoints > userPoints) {
+        gameWinner = (`The computer wins the game!`);
+        hideButtons();
+        showResetButton();   
+    } else if (roundCount === 5 && userPoints === computerPoints) {
+        gameWinner = (`It's a tie, baby!`);
+        hideButtons();
+        showResetButton();
     }
-
-    //announce game winner at the end of the game
-    if(userPoints > computerPoints) {
-        return `You win the game! Congrats! Refresh the browser for another game`;
-    }else if(computerPoints > userPoints) {
-        return `The computer wins the game! Refresh the browser for another game`;
-    }else if(userPoints === computerPoints){
-        return `It's a tie, baby! Refresh the browser for another game`
-    }
+    document.getElementById('game-winner').innerHTML = gameWinner;
 }
 
-alert(game());
-
-
-
-
- 
+//playerSelector === button.id
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+        game();
+    });
+});
